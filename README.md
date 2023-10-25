b67f6408a0b4b92fedd4cbb633aa2f05dd77a347
Zero <mohitgoyal.mg@proton.me>
2023-10-25 21:37:53 +0530

    Updated the code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Crypter
+
+A website to encrypt/decrypt text and files to keep them secure while sharing acrossing web or uploading to cloud.
--- app.py
+++ app.py
@@ -1,100 +1,160 @@
-import requests
-import tkinter
-from tkinter import messagebox
+from flask import Flask, request, jsonify, make_response, render_template, redirect, url_for
+import jwt
+import math
+import pymongo
+import time
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class Application:
-	windows_bg = '#333'
-	windows_fg = '#CCC'
-
-	def __init__(self):
-		self.window = tkinter.Tk()
-		self.window.title('Login')
-		self.window.geometry('340x440')
-		self.window.configure(bg=self.windows_bg)
-
-		frame = tkinter.Frame(self.window, bg=self.windows_bg)
-
-		# Widgets
-		login_label = tkinter.Label(frame, text='Sign in', bg=self.windows_bg, fg='#F39', font=('Helvetica', 22))
-
-		username_label = tkinter.Label(frame, text='Username', bg=self.windows_bg, fg=self.windows_fg, font=('Helvetica', 16))
-		username_entry = tkinter.Entry(frame)
-		
-		password_label = tkinter.Label(frame, text='Password', bg=self.windows_bg, fg=self.windows_fg, font=('Helvetica', 16))
-		password_entry = tkinter.Entry(frame, show='*')
-		
-		login_button = tkinter.Button(frame, text='Login', bg=self.windows_bg, fg='#F39', font=('Helvetica', 16), command=self.login)
-
-		register_label = tkinter.Label(frame, text='New to the app?', bg=self.windows_bg, fg=self.windows_fg, font=('Helvetica', 12))
-		register_button = tkinter.Button(frame, text='Register here', bg=self.windows_bg, fg='#F39', font=('Helvetica', 12), border=0, command=self.new_user)
+# from utils import encrypt, decrypt, login_user, verify_login
 
 
-		# Widget placement
-		login_label.grid(row=0, column=0, columnspan=2, pady=30)
+mongo = pymongo.MongoClient('localhost', 27017)
 
-		username_label.grid(row=1, column=0, pady=20)
-		username_entry.grid(row=1, column=1)
-		
-		password_label.grid(row=2, column=0, pady=20)
-		password_entry.grid(row=2, column=1)
-		
-		login_button.grid(row=3, column=0, columnspan=2, pady=30)
-		
-		register_label.grid(row=4, column=0)
-		register_button.grid(row=4, column=1)
-		
-		frame.pack()
+app = Flask(__name__)
+app.secret_key = 'secret_key'
 
-		self.window.mainloop()
 
+
+def login_user(response, user):
+	token = jwt.encode({**user, 'expiration': time.time()+86400}, app.secret_key, algorithm='HS256')
+
+	response.headers.set('Authorization', f"Bearer {token}")
+
+	return response
+
+
+def verify_login(token):
+	details = jwt.decode(token.lstrip('Bearer '), app.secret_key, algorithm='HS256')
 	
-	def login(self):
-		if not True:
-			messagebox.showinfo(title='Success', message='Successfully Logged In')
-		
-		else:
-			messagebox.showerror(title='Failed', message='Invalid login details')
+	if 'username' in details and mongo['local']['user'].find({'username': details['username']}):
+		return True
+
+	return False
 
 
-	def new_user(self):
-		screen = tkinter.Toplevel(self.window, bg=self.windows_bg)
-		screen.geometry('340x340+200+200')
-		screen.title('Register')
-
-		frame = tkinter.Frame(screen, bg=self.windows_bg)
-
-		# Widgets
-		register_label = tkinter.Label(frame, text='Register', bg=self.windows_bg, fg='#F39', font=('Helvetica', 22))
-
-		username_label = tkinter.Label(frame, text='Username', bg=self.windows_bg, fg=self.windows_fg, font=('Helvetica', 16))
-		username_entry = tkinter.Entry(frame)
-		
-		password_label = tkinter.Label(frame, text='Password', bg=self.windows_bg, fg=self.windows_fg, font=('Helvetica', 16))
-		password_entry = tkinter.Entry(frame, show='*')
-		
-		register_button = tkinter.Button(frame, text='Sign Up', bg=self.windows_bg, fg='#F39', font=('Helvetica', 16), command=self.register)
-
-		# Widget placement
-		register_label.grid(row=0, column=0, columnspan=2, pady=30)
-
-		username_label.grid(row=1, column=0, pady=20)
-		username_entry.grid(row=1, column=1)
-		
-		password_label.grid(row=2, column=0, pady=20)
-		password_entry.grid(row=2, column=1)
-		
-		register_button.grid(row=3, column=0, columnspan=2, pady=30)
-		
-		frame.pack()
-
-		screen.mainloop()
+def get_hash(token):
+	return jwt.decode(token.lstrip('Bearer '), app.secret_key, algorithm='HS256')['password']
 
 
-	def register(self):
+def matrixize(data, matrix=[]):
+	if len(data) == 1:
+		return matrix.append([data])
+	
+	size = math.sqrt(len(data))
+
+	return matrixize(data[size:], matrix.append(data[:size]))
+
+
+def obfuscate(data):
+	obfuscated_string = ''
+
+	while data:
+		for x in data:
+			if len(data[x]) > 0:
+				obfuscated_string = obfuscated_string + data[x].pop(0)
+
+			data.pop(x)
+
+	return obfuscated_string
+
+
+def encrypt(data, token, key, source_type):
+	if isinstance(data, str) and source_type == 'text':
+		data = matrixize(data + get_hash(token))
+
+	else:
 		pass
 
 
-if __name__ == '__main__':
-	app = Application()
-	app.login()
+def decrypt(data, token, key, source_type):
+	if isinstance(data, str) and source_type == 'text':
+		pass
+
+	else:
+		pass
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+	# logout_user()
+	return redirect(url_for('/'))
+
+
+@app.route('/', methods=['GET'])
+def index():
+	if request.method == 'GET':
+		return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+	if request.method == 'POST':
+		payload = request.form
+
+		if 'username' in payload and 'password' in payload:
+			if mongo['local']['user'].find_one({'username': payload['username']}):
+				response = jsonify({
+					'status': 406,
+					'message': 'A user with this username/e-mail already exists, please login'
+				})
+
+				response.status_code = 406
+
+				return response
+
+			mongo['local']['user'].insert_one({'username': payload['username'], 'password': generate_password_hash(payload['password'])})
+
+			response = make_response(render_template('crypter.html'))
+			response = login_user(response, {'username': payload['username'], 'password': generate_password_hash(payload['password'])})
+			
+
+			print(response)
+			
+			return response
+		
+		else:
+			response = jsonify('Invalid details for user registation, please try again')
+			response.status_code = 403
+
+			return response
+		
+
+@app.route('/encrypt/text', methods=['POST'])
+def encrypt_text():
+	response = jsonify({
+		'status': 200
+	})
+	
+	return response
+
+
+@app.route('/encrypt/file', methods=['POST'])
+def encrypt_file():
+	return redirect(url_for('/crypter'))
+
+
+@app.route('/decrypt/text', methods=['POST'])
+def decrypt_text():
+	return redirect(url_for('/crypter'))
+
+
+@app.route('/decrypt/file', methods=['POST'])
+def decrypt_file():
+	return redirect(url_for('/crypter'))
+
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+	message = jsonify({
+		'status': 404,
+		'message': request.url + ' Not Found'
+	})
+
+	message.status_code = 404
+
+	return message
+
+
+app.run(debug=True)
